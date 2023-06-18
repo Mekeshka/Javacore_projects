8a359595870efd85e97e9ccc54bb40d07fa14b51
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-06-18 12:30:10 +0300

    Add class ConsoleWriter. Add enter number and sight from keybord

---
+++ src/main/java/ru/sokolova/task2_2/action/ConsoleWriter.java
@@ -0,0 +1,9 @@
+package ru.sokolova.task2_2.action;
+
+public class ConsoleWriter {
+    public static void writeResult (String operation, String sign, int result, int x, int y){
+        System.out.println(operation + x + sign + y + " = " + result);
+
+    }
+
+}
--- src/main/java/ru/sokolova/task2_2/action/ResultPrinter.java
+++
@@ -1,7 +0,0 @@
-/*
-package ru.sokolova.task2_2.action;
-
-public class ResultPrinter {
-    System.out.println ()
-}
-*/
