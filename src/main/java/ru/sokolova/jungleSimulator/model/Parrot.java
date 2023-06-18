070b75d1c6a3dfb76acd53cafe70243024b18d9d
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-06-18 11:16:57 +0300

    Refactor energyProductionCoefficient

--- src/main/java/ru/sokolova/jungleSimulator/model/Parrot.java
+++ src/main/java/ru/sokolova/jungleSimulator/model/Parrot.java
@@ -3,23 +3,29 @@ package ru.sokolova.jungleSimulator.model;
 public class Parrot {
     private int health = 100;
     private int energy = 100;
+
     private final double energyProductionCoefficient = 1.5;
 
-    public void setHealth(int health){
-        this.health=health;
+
+    public void setHealth(int health) {
+        this.health = health;
     }
-    public int getHealth();
+
+    public int getHealth() {
         return this.health;
-
-    public void setEnergy(int energy){
-        this.energy=energy;
     }
-    public int getEnergy(){
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getEnergy() {
         return this.energy;
-
     }
-    public double getBeak(){
+
+    public double getBeak() {
         return this.energyProductionCoefficient;
     }
+}
 
 
