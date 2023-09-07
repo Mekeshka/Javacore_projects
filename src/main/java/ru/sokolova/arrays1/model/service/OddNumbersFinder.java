82bd12e7a2c9fcf8f491813320bb87a28a91f72f
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-07 19:57:09 +0300

    Odd numbers finder method add

---
+++ src/main/java/ru/sokolova/arrays1/model/service/OddNumbersFinder.java
@@ -0,0 +1,17 @@
+package ru.sokolova.arrays1.model.service;
+
+import java.sql.SQLOutput;
+
+public class OddNumbersFinder {
+    public int findOddNumber(int[] arr) {
+        int oddCounter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                oddCounter++;
+
+            }
+        }
+        System.out.println(oddCounter);
+        return oddCounter;
+    }
+}
