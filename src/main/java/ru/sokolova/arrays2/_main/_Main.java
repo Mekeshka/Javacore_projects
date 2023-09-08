9fdd66e5683359ebde5436624c5f285314057a5e
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-08 15:25:04 +0300

    Main class and method add to arrays2 task

--- src/main/java/ru/sokolova/arrays2/_main/_Main.java
+++ src/main/java/ru/sokolova/arrays2/_main/_Main.java
@@ -1,7 +1,11 @@
 package ru.sokolova.arrays2._main;
 
+import ru.sokolova.arrays2.view.AscSortPrinter;
+
 public class _Main {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{2, -3, 1, 1};
+        AscSortPrinter ascSortPrinter = new AscSortPrinter();
+        ascSortPrinter.printSortedArray(arr);
     }
 }
