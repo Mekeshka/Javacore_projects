4b1654cfb0b549591bdd290dc18f0724facc19e3
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-08 15:25:47 +0300

    Asceending sort method add to arrays2 task

--- src/main/java/ru/sokolova/arrays2/model/service/AscendingSort.java
+++ src/main/java/ru/sokolova/arrays2/model/service/AscendingSort.java
@@ -1,6 +1,18 @@
 package ru.sokolova.arrays2.model.service;
 
 public class AscendingSort {
-    public int sortAsc(int arr[]){
+
+
+    public int[] sortAsc(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int buffer = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = buffer;
+                }
+            }
+        }
+        return arr;
     }
 }
