a161e048b78ffbbfb48641bf2b88e201a66b7c2b
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-08 15:26:33 +0300

    Print in console method add to arrays2 task

---
+++ src/main/java/ru/sokolova/arrays2/view/AscSortPrinter.java
@@ -0,0 +1,13 @@
+package ru.sokolova.arrays2.view;
+
+import ru.sokolova.arrays2.model.service.AscendingSort;
+
+import java.util.Arrays;
+
+public class AscSortPrinter {
+    public void printSortedArray(int arr[]) {
+        AscendingSort ascendingSort = new AscendingSort();
+
+        System.out.println(Arrays.toString(ascendingSort.sortAsc(arr)));
+    }
+}
