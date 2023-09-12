0c54621dc239a3b068a655a0d09e1fcb8e5984b1
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-12 16:01:25 +0300

    countSumNumbers method add

--- src/main/java/ru/sokolova/arrays1/model/service/SumNumberCounter.java
+++ src/main/java/ru/sokolova/arrays1/model/service/SumNumberCounter.java
@@ -2,13 +2,10 @@ package ru.sokolova.arrays1.model.service;
 
 public class SumNumberCounter {
     public int countNumberSum(int arr[]) {
-        int sum1 = 0;
-        int sum2=0;
         int numberSum=0;
-        for (int i = 0; i < arr.length-1; i++) {
-            numberSum = arr[i] + arr[i + 1]+numberSum;
+        for (int i = 0; i < arr.length; i++) {
+            numberSum=numberSum+arr[i];
         }
-        System.out.println(numberSum);
         return numberSum;
     }
 }
