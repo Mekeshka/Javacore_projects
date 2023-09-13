3100f53fb39f381550fd584bb2c35b2c3c77cf7f
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-13 13:44:56 +0300

    countNumberDifference method add

--- src/main/java/ru/sokolova/arrays1/model/service/DifferenceNumberCounter.java
+++ src/main/java/ru/sokolova/arrays1/model/service/DifferenceNumberCounter.java
@@ -1,18 +1,18 @@
 package ru.sokolova.arrays1.model.service;
 
 public class DifferenceNumberCounter {
-    public int countDifferenceNumber(int arr[]){
-        int oddIndexSum=0;
-        int evenIndexSum=0;
-        int difference=oddIndexSum-evenIndexSum;
-        for (int i=1; i<arr.length; i++){
-            if (i%2==0){
-                oddIndexSum=arr[i]+oddIndexSum;
+    public int countDifferenceNumber(int arr[]) {
+        int oddIndexSum = 0;
+        int evenIndexSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                oddIndexSum = arr[i] + oddIndexSum;
             }
-            if (i%2!=0){
-                evenIndexSum=arr[i]+evenIndexSum;
+            if (i % 2 != 0) {
+                evenIndexSum = arr[i] + evenIndexSum;
             }
         }
+        int difference = oddIndexSum - evenIndexSum;
         return difference;
     }
 }
