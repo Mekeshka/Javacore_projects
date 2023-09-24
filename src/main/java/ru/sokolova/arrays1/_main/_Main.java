9a8b0ba0ba76d603980acbcce8ca5e39cfaea2b5
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-09-24 13:43:49 +0300

    Main class add methods call

--- src/main/java/ru/sokolova/arrays1/_main/_Main.java
+++ src/main/java/ru/sokolova/arrays1/_main/_Main.java
@@ -1,11 +1,19 @@
 package ru.sokolova.arrays1._main;
 
+import ru.sokolova.arrays1.model.service.ArrayService;
 import ru.sokolova.arrays1.view.ResultPrinter;
 
 public class _Main {
     public static void main(String[] args) {
-        int[] array = new int[]{5, -3, 2, 1, 7, 11, 12, -4, 1, 10};
+        int[] array = new int[]{5, 3, 4, 6, 10, 0, -2, 3};
+        ArrayService arrayService = new ArrayService();
         ResultPrinter resultPrinter = new ResultPrinter();
-        resultPrinter.printResult(array);
+        arrayService.countOddNumber(array);
+        resultPrinter.printResult(result);
+        arrayService.countEvenNumber(array);
+//        arrayService.countPrimeNumber(array);
+        arrayService.countNumberSum(array);
+        arrayService.countDifferenceNumber(array);
+        arrayService.countZeroNumbers(array);
     }
 }
--- src/main/java/ru/sokolova/arrays1/view/ResultPrinter.java
+++ src/main/java/ru/sokolova/arrays1/view/ResultPrinter.java
@@ -1,19 +1,7 @@
 package ru.sokolova.arrays1.view;
 
-import ru.sokolova.arrays1.model.service.*;
-
 public class ResultPrinter {
-    public void printResult(int arr[]){
-        OddNumbersCounter oddNumbersCounter = new OddNumbersCounter();
-        EvenNumbersCounter evenNumbersCounter = new EvenNumbersCounter();
-//      PrimeNumberCounter primeNumberCounter=new PrimeNumberCounter();
-        SumNumberCounter sumNumberCounter=new SumNumberCounter();
-        DifferenceNumberCounter differenceNumberCounter = new DifferenceNumberCounter();
-        ZeroNumbersCounter zeroNumbersCounter = new ZeroNumbersCounter();
-        System.out.println(oddNumbersCounter.countOddNumber(arr));
-        System.out.println(evenNumbersCounter.countEvenNumber(arr));
-        System.out.println(sumNumberCounter.countNumberSum(arr));
-        System.out.println(differenceNumberCounter.countDifferenceNumber(arr));
-        System.out.println(zeroNumbersCounter.countZeroNumbers(arr));
+    public void printResult(int result) {
+        System.out.println(result);
     }
 }
