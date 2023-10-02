107ee3592dbafdc0eb530b984b56591e2d787920
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-10-02 21:26:53 +0300

    first version of Citizen class

--- src/main/java/ru/sokolova/state/model/entity/Citizen.java
+++ src/main/java/ru/sokolova/state/model/entity/Citizen.java
@@ -1,4 +1,26 @@
 package ru.sokolova.state.model.entity;
 
 public class Citizen {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private State state;
+    public Citizen(int id, String name, String surname, int age, State state) {
+        this.id = id;
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.state=state;
+    public int getId(){
+        return this.id;
+        }
+    public void setId(int id){
+        this.id=id;
+        }
+    public String getName(){
+        return this.name;
+        }
+    }
 }
+
