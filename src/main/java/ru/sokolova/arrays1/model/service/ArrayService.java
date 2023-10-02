9e58c2e77eea0f30ed63f629f017fd100fa59a7f
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-10-02 20:07:13 +0300

    add all count methods into one common service class ArrayService

--- src/main/java/ru/sokolova/arrays1/model/service/ArrayService.java
+++ src/main/java/ru/sokolova/arrays1/model/service/ArrayService.java
@@ -22,6 +22,11 @@ public class ArrayService {
     public int countPrimeNumber(int arr[]) {
         int primeNumberCounter = 0;
         for (int i = 0; i < arr.length; i++) {
+            for (int j =0; j<arr.length;j++){
+                if (arr[j]<2){
+                    break;
+                }
+            }
             for (int k = 2; k <= Math.sqrt(arr[i]); k++) {
                 if (arr[i] % k == 0) {
                     break;
